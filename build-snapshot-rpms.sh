cde04acd31683d194bd21929e72d2cbcb9fc6b90
Konrad Kleine <kkleine@redhat.com>
2021-02-08 22:58:43 +0100

    Fix location of mock cfg

--- build-snapshot-rpms.sh
+++ build-snapshot-rpms.sh
@@ -91,7 +91,7 @@ curl -R -L https://github.com/llvm/llvm-project/archive/${latest_git_sha}.tar.gz
   | tar -C ${llvm_src_dir} --strip-components=1 -xzf -
 
 for proj in $projects; do
-    tarball_path=rpms/$proj/$proj-${snapshot_name}.src.tar.xz
+    tarball_path=${cur_dir}/rpms/$proj/$proj-${snapshot_name}.src.tar.xz
     project_src_dir=${llvm_src_dir}/$proj-${snapshot_name}.src
     echo "Creating tarball for $proj in $tarball_path from $project_src_dir ..."
     mv $llvm_src_dir/$proj $project_src_dir
@@ -124,7 +124,7 @@ for proj in $projects; do
     spectool -R -g -A -C . $proj.spec
 
     # Build SRPM
-    time mock -r rawhide-mock.cfg \
+    time mock -r ${cur_dir}/rawhide-mock.cfg \
         --spec=$proj.spec \
         --sources=$PWD \
         --buildsrpm \
@@ -133,7 +133,7 @@ for proj in $projects; do
         --isolation=simple
 
     # Build RPM
-    time mock -r rawhide-mock.cfg \
+    time mock -r ${cur_dir}/rawhide-mock.cfg \
         --rebuild $out_dir/srpms/${proj}-${llvm_version}-0.${snapshot_name}.fc${fc_version}.src.rpm \
         --resultdir=$out_dir/rpms \
         --no-cleanup-after \
