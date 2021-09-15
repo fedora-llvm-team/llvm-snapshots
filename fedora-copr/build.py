ae0fcdebb543aa8f94df1d9b784e9477fd07f1af
Konrad Kleine <kkleine@redhat.com>
2021-09-15 21:53:31 +0200

    Add note about compat packages being in a separate copr project

--- fedora-copr/build.py
+++ fedora-copr/build.py
@@ -51,6 +51,8 @@ class CoprBuilder(object):
             # build for those chroots.
         else:
             print("Creating project {}/{}".format(self.ownername, self.projectname))
+            # TODO(kwk): Outsource compat packages into their own copr project
+            # and make it an additional_repos=
             self.client.project_proxy.add(
                 ownername=self.ownername,
                 projectname=self.projectname,
