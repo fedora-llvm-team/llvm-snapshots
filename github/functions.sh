3556dd10c6650e165348eb86774a653b56eaf64b
Konrad Kleine <kkleine@redhat.com>
2023-09-22 17:08:37 +0200

    Ensure all combinations of package+chroot exist
    This ensures that all combinations of packages and chroots that we care
    about, do exist in in the monitor of a project.
    
    Before we just checked for something other than `succeeded` builds but
    it could be that in a project there's no build at all or a build is
    missing. Then we don't want to wave a build through.

--- github/functions.sh
+++ github/functions.sh
@@ -1,4 +1,4 @@
-set -x
+set +x
 # set -e
 # TODO(kwk): Is there a better way to check project existence?
 # TODO(kwk): Maybe: copr list $username | grep --regexp="^Name: \$project$"
@@ -15,8 +15,43 @@ function get_active_build_ids(){
     | cut -f 1
 }
 
-function has_not_succeeded_builds(){
+# Prints the chroots we care about.
+function get_chroots() {
+  copr list-chroots | grep -P '^fedora-(rawhide|[0-9]+)' | tr '\n' ' '
+}
+
+# Prints the packages we care about
+function get_packages() {
+  echo "python-lit llvm clang lld compiler-rt libomp"
+}
+
+# Returns true if a package needs special handling on certain architectures
+function is_package_supported_by_chroot() {
+  local pkg=$1;
+  local chroot=$2;
+
+  if [[ ("$pkg" == "lld" || "$pkg" == "libomp") && $chroot =~ -s390x$ ]]; then
+    false
+  else
+    true
+  fi
+}
+
+# Returns 0 if all packages on all* chroots have successful builds.
+#
+# *: All supported combinations of package + chroot (see is_package_supported_by_chroot).
+function has_all_good_builds(){
   local project=$1;
-  copr monitor --output-format text-row $project \
-    | grep -v succeeded
+
+  copr monitor --output-format text-row --fields state,chroot,name $project | sort -k1 -k2 -k3 > /tmp/actual.txt
+  truncate -s 0 /tmp/expected.txt
+  for chroot in $(get_chroots); do
+    for package in $(get_packages) llvm-snapshot-builder; do
+      if is_package_supported_by_chroot "$package" "$chroot"; then
+        echo "succeeded $chroot $package" >> /tmp/expected.txt
+      fi
+    done
+  done
+  sort -k1 -k2 -k3 -o /tmp/expected.txt /tmp/expected.txt
+  diff -bus /tmp/expected.txt /tmp/actual.txt
 }
