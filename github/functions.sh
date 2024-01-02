5db2ecba20bf8a4cdad55afa9f7cfd3280ea8662
Konrad Kleine <kkleine@redhat.com>
2024-01-02 13:08:30 +0100

    check-snapshots workflow: use matrix strategy
    I found myself duplicating things again for the upcoming bootstrap
    builds and figured it is best to employ a matrix strategy for
    checking for snapshots. That is easily extensible. Just consider moving
    from big-merge to bootstrap and then to PGO. All will benefit from this
    change. I'll do the same for the rest of the jobs soon.
    
    Additionally I've added strategy labels (`strategy/standalone` and
    `strategy/big-merge`) labels to issues that report a broken snapshot
    build. This allows us to better filter for already existing issues.

--- github/functions.sh
+++ github/functions.sh
@@ -1,9 +1,3 @@
-# This is a duplicate of the functions.sh file with only slight modifications:
-# We only want these packages: python-lit, llvm, lld. And we only want one
-# chroot for now.
-#
-# TODO(kkleine): Remove me once big merge is a reality
-
 set +x
 
 # Prints the year month and day combination for today
@@ -15,7 +9,10 @@ function yyyymmdd() {
 function was_broken_snapshot_detected_today() {
   local d=`yyyymmdd`
   gh --repo ${GITHUB_REPOSITORY} issue list \
-    --label broken_snapshot_detected --state all | grep -P "$d detected\s+[^\(]" > /dev/null 2>&1
+    --label broken_snapshot_detected \
+    --label standalone \
+    --state all \
+  | grep -P "$d" > /dev/null 2>&1
 }
 
 # Checks if a copr project exists
