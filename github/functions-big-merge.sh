aced1255a04132e1c6d11e5b007cf679eeb379c8
Konrad Kleine <kkleine@redhat.com>
2024-01-10 14:39:01 +0100

    Remove no longer needed specialized version of was_broken_snapshot_detected_today

--- github/functions-big-merge.sh
+++ github/functions-big-merge.sh
@@ -1,17 +1,6 @@
 # This file contains overwrite for the functions in functions.sh
 set +x
 
-# overwrite
-function was_broken_snapshot_detected_today() {
-  local d=`yyyymmdd`
-  gh --repo ${GITHUB_REPOSITORY} issue list \
-    --label broken_snapshot_detected \
-    --label strategy/big-merge \
-    --state all \
-    --search "$d" \
-  | grep -P "$d" > /dev/null 2>&1
-}
-
 # Prints the chroots we care about.
 prefix_function get_chroots
 function get_chroots() {
