92c8dda1dcc6b1af8aba26f5fe59f6f4113816a5
Konrad Kleine <kkleine@redhat.com>
2024-01-10 14:32:26 +0100

    copr: add rhel-9-x86_64 build root as an experiment (#175)
    * copr: add rhel-9-x86_64 build root as an experiment
    
    Co-authored-by: Nikita Popov <npopov@redhat.com>

--- github/functions-big-merge.sh
+++ github/functions-big-merge.sh
@@ -1,6 +1,13 @@
 # This file contains overwrite for the functions in functions.sh
 set +x
 
+# Prefixes the old version of the given function with "_" to make it callable
+# from the overwriting function.
+function overwrite_function() {
+  local function_name=$1
+  eval "_`declare -f $function_name`"
+}
+
 # overwrite
 function was_broken_snapshot_detected_today() {
   local d=`yyyymmdd`
@@ -10,6 +17,13 @@ function was_broken_snapshot_detected_today() {
     --state all \
     --search "$d" \
   | grep -P "$d" > /dev/null 2>&1
+}
+
+# Prints the chroots we care about.
+overwrite_function get_chroots
+function get_chroots() {
+  _get_chroots
+  echo -n "rhel-9-x86_64 "
 }
 
 # Prints the packages we care about
