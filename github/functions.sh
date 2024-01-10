26a0a48886aa816a8c1ead8fbeed5addcef12966
Konrad Kleine <kkleine@redhat.com>
2024-01-10 16:16:05 +0100

    Automatically identify build failure causes (#174)
    This allows us to continously monitor a given day for error causes.
    
    When a new issue is created for a broken snapshot build, we'll check the
    logs of the failing builds for anything that we identify as an error.
    The errors we can identify so far are listed below. A github issue label (i.e. `error/copr_timeout`, `error/network_issue`) is then automatically added to the broken snapshot issue.
    
    # Examples of error causes
    
    ## `copr_timeout`
    
    <pre>
     <b>!! Copr timeout</b> => sending INT
    WARNING: Machine 5851d3319caf45608112626e5dff90dc still running. Killing...
    Copr build error: Build failed
    </pre>
    
    ## `network_issue`
    
    <pre>
    <b>Errors during downloading metadata for repository</b> 'local':
      - Status code: 404 for https://kojipkgs.fedoraproject.org/repos/rawhide/latest/x86_64/repodata/c3262644a11234a683575d44416933749f6808ea60694a499b11336c3b735223-filelists.xml.gz (IP: 38.145.60.20)
      - Status code: 404 for https://kojipkgs.fedoraproject.org/repos/rawhide/latest/x86_64/repodata/729723bef89105229ca2204cb1fdb395e04630c92e8119a2a3eec1c70dcb1a62-primary.xml.gz (IP: 38.145.60.20)
    Error: Failed to download metadata for repo 'local': Yum repo downloading error: Downloading error(s): repodata/729723bef89105229ca2204cb1fdb395e04630c92e8119a2a3eec1c70dcb1a62-primary.xml.gz - Cannot download, all mirrors were already tried without success; repodata/c3262644a11234a683575d44416933749f6808ea60694a499b11336c3b735223-filelists.xml.gz - Cannot download, all mirrors were already tried without success
    </pre>
    
    ## `test_issue`
    
    <pre>
    PASS: lit :: shtest-define.py (73079 of 73079)
    ********************
    <b>Failed Tests</b> (1):
      libarcher :: races/task-taskgroup-unrelated.c
    
    
    Testing Time: 226.85s
    
    <b>Total Discovered Tests:</b> 96435
      Skipped          :    50 (0.05%)
      Unsupported      :  2416 (2.51%)
      Passed           : 93787 (97.25%)
      Expectedly Failed:   181 (0.19%)
      Failed           :     1 (0.00%)
    </pre>
    
    ## `dependency_issue`
    
    <pre>
    No matching package to install: 'llvm-googletest = 18.0.0~pre20240108.g60c4f82d3c4e9c'
    No matching package to install: 'llvm-static = 18.0.0~pre20240108.g60c4f82d3c4e9c'
    No matching package to install: 'llvm-test = 18.0.0~pre20240108.g60c4f82d3c4e9c'
    <b>Not all dependencies satisfied</b>
    Error: Some packages could not be found.
    WARNING: DNF command failed, retrying, attempt #2, sleeping 10s
    No matches found for the following disable plugin patterns: local, spacewalk, versionlock
    </pre>
    
    ## `unknown`
    
    Anything that we cannot identify as one of the above error causes will be classified as `unknown`. Those are probably the most interesting errors to look into. That is why this change materializes the tail of the build log as a comment on the issue related to a broken snapshot.

--- github/functions.sh
+++ github/functions.sh
@@ -7,13 +7,29 @@ function yyyymmdd() {
 
 # Checks if there's an issue for a broken snapshot reported today
 function was_broken_snapshot_detected_today() {
+  local repo=$1
+  local strategy=$2
   local d=`yyyymmdd`
-  gh --repo ${GITHUB_REPOSITORY} issue list \
+  gh --repo $repo issue list \
     --label broken_snapshot_detected \
-    --label strategy/standalone \
+    --label strategy/$strategy \
     --state all \
     --search "$d" \
   | grep -P "$d" > /dev/null 2>&1
+}
+
+# Get today's issue. Make sure was_broken_snapshot_detected_today found one.
+function todays_issue_number() {
+  local repo=$1
+  local strategy=$2
+  local d=`yyyymmdd`
+  gh --repo $repo issue list \
+    --label broken_snapshot_detected \
+    --label strategy/$strategy \
+    --state all \
+    --search "$d" \
+    --json number \
+    --jq .[0].number
 }
 
 # Checks if a copr project exists
@@ -80,10 +96,223 @@ function has_all_good_builds(){
   diff -bus /tmp/expected.txt /tmp/actual.txt
 }
 
+# Prints a comment on a given github issue that begins with an HTML comment
+# prefix in the form: <!--error/$cause project/$project chroot/$chroot-->.
+#
+# If no such comment exists, nothing is printed.
+#
+# Here's an example of what is printed.
+#
+#   {
+#     "author": {
+#       "login": "kwk"
+#     },
+#     "authorAssociation": "OWNER",
+#     "body": "<!--error_causes-->\r\nFoobar.",
+#     "createdAt": "2023-04-12T12:34:19Z",
+#     "id": "IC_123412341234",
+#     "includesCreatedEdit": true,
+#     "isMinimized": false,
+#     "minimizedReason": "",
+#     "reactionGroups": [],
+#     "url": "https://github.com/<USER>/<REPO>/issues/1#issuecomment-1505197645",
+#     "viewerDidAuthor": true
+#   }
+function get_error_cause_comment() {
+  local user_repo=$1
+  local issue_number=$2
+  local project=$3
+  local chroot=$4
+  local cause=$5
+
+  gh --repo $user_repo issue view $issue_number \
+    --comments \
+    --json comments \
+    --jq '.comments[] | select(.body | startswith("<!--error/'$cause' project/'$project' chroot/'$chroot'-->"))'
+}
+
+# Checks if a comment exists on a given github issue that begins with an HTML
+# comment prefix in the form:
+#   <!--error/$cause project/$project chroot/$chroot-->.
+function has_error_cause_comment() {
+  local user_repo=$1
+  local issue_number=$2
+  local project=$3
+  local chroot=$4
+  local cause=$5
+
+  comment=$(get_error_cause_comment $user_repo $issue_number $project $chroot $cause)
+  [[ -n "$comment" ]]
+}
+
+# For a given project this function prints causes for all cases of errors it can
+# automatically identify (e.g. copr_timeout, network_issue). The caller needs to
+# make sure the labels exists before adding them to an issue. If you pass an
+# additional file name, we will write every error cause with additional
+# information to it
+# (<cause>;<package_name>;<chroot>;<build_log_url>;<path_to_context_file>). The
+# context file contains the lines before and after the error in the build log.
+function list_error_causes(){
+  local project=$1;
+  local causes_file=$2;
+  local grep_opts="-n --context=3"
+  local monitor_file=$(mktemp)
+  local context_file=$(mktemp)
+
+  [[ -n "$causes_file" ]] && truncate --size 0 $causes_file
+
+  copr monitor \
+    --output-format json \
+    --fields chroot,name,state,url_build_log $project \
+    > $monitor_file
+
+  cat $monitor_file | jq -r '.[] | select(.state | contains("failed")) | to_entries | map(.value) | @tsv' \
+  | while IFS=$'\t' read -r chroot package_name state build_log_url; do
+    # echo "state=$state package_name=$package_name chroot=$chroot build_log_url=$build_log_url";
+
+    log_file=$(mktemp)
+    curl -sL $build_log_url | gunzip -c  > $log_file
+
+    got_cause=0
+
+    function store_cause() {
+      local cause=$1
+      echo $cause
+      if [[ -n "$causes_file" ]]; then
+        echo "$cause;$package_name;$chroot;$build_log_url;$context_file" >> $causes_file
+        # For the next error we need to make room an create a new context file
+        context_file=$(mktemp)
+      fi
+      got_cause=1
+    }
+
+    # Check for timeout
+    if [ -n "$(grep $grep_opts '!! Copr timeout' $log_file | tee $context_file)" ]; then
+      store_cause "copr_timeout"
+    fi
+
+    # Check for network issues
+    if [ -n "$(grep $grep_opts 'Errors during downloading metadata for repository' $log_file | tee $context_file)" ]; then
+      store_cause "network_issue"
+    fi
+
+    # Check for dependency issues
+    if [ -n "$(grep $grep_opts 'Not all dependencies satisfied' $log_file | tee $context_file)" ]; then
+      store_cause "dependency_issue"
+    fi
+
+    # Check for test issues
+    if [ -n "$(pcre2grep -n --before-context=2 --after-context=10 -M '(Failed Tests|Unexpectedly Passed Tests).*(\n|.)*Total Discovered Tests:' $log_file | tee $context_file)" ]; then
+      store_cause "test"
+    fi
+
+    # TODO: Feel free to add your check here (make sure to set got_cause=1)...
+
+    if [ "$got_cause" == "0" ]; then
+      # Add the tail of the log to the context file because we haven't got any better information
+      tail -n 10 > $context_file
+      store_cause "unknown"
+    fi
+
+    rm $log_file
+  done | sort | uniq
+}
+
+# Takes a file with error causes and promotes unknown build causes as their own
+# comment on the given issue.
+#
+# A causes file looks like a semicolon separated list file:
+#
+#  network_issue;llvm;fedora-rawhide-i386;https://download.copr.fedorainfracloud.org/results/@fedora-llvm-team/llvm-snapshots-big-merge-20240105/fedora-rawhide-i386/06865034-llvm/builder-live.log.gz;/tmp/tmp.v17rnmc4rp
+#  copr_timeout;llvm;fedora-39-ppc64le;https://download.copr.fedorainfracloud.org/results/@fedora-llvm-team/llvm-snapshots-big-merge-20240105/fedora-39-ppc64le/06865030-llvm/builder-live.log.gz;/tmp/tmp.PMXc0b7uEE
+function report_build_issues() {
+  local repo=$1
+  local issue_number=$2;
+  local causes_file=$3;
+  local maintainer_handle=$4;
+  local comment_body_file=""
+
+  while IFS=';' read -r cause package_name chroot build_log_url context_file;
+  do
+    echo "$cause $package_name $chroot $build_log_url $context_file"
+    if [ "$cause" == "network_issue" ]; then
+      if ! has_error_cause_comment $repo $issue_number $package_name $chroot $cause ; then
+        comment_body_file=$(mktemp)
+
+        cat <<EOF > $comment_body_file
+<!--error/$cause project/$package_name chroot/$chroot-->
+@maintainer_handle, package \`$package_name\` failed to build on \`$chroot\` for an unknown cause ([build-log]($build_log_url)):
+
+\`\`\`
+$(cat $context_file)
+\`\`\`
+EOF
+        gh --repo $repo issue comment $issue_number --body-file $comment_body_file
+        rm $comment_body_file
+      else
+        echo "error cause comment for project '$package_name' and '$chroot' and '$cause' already exists"
+      fi
+    fi
+  done < $causes_file
+}
+
+#region labels
+
+# Iterates over the given labels and creates or edits each label in the list
+# with the given prefix and color.
+function _create_labels() {
+  local repo=$1
+  local labels=$2
+  local label_prefix=$3
+  local color=$4
+
+  for label in $labels; do
+    gh --repo $repo label create $label_prefix$label --color $color --force
+  done
+}
+
+function create_labels_for_error_causes() {
+  local repo=$1
+  local error_causes=$2
+  _create_labels $repo $error_causes "error/" "F76B19"
+}
+
+function create_labels_for_archs() {
+  local repo=$1
+  local archs=$2
+  _create_labels $repo $archs "arch/" "C5DEF5"
+}
+
+function create_labels_for_oses() {
+  local repo=$1
+  local oses=$2
+  _create_labels $repo $oses "os/" "F9D0C4"
+}
+
+function create_labels_for_projects() {
+  local repo=$1
+  local projects=$2
+  _create_labels $repo $projects "project/" "BFDADC"
+}
+
+function create_labels_for_strategies() {
+  local repo=$1
+  local strategies=$2
+  _create_labels $repo $strategies "strategy/" "FFFFFF"
+}
+#endregion
+
 # This installs the gh client for Fedora as described here:
 # https://github.com/cli/cli/blob/trunk/docs/install_linux.md#fedora-centos-red-hat-enterprise-linux-dnf
 function install_gh_client() {
   dnf install -y 'dnf-command(config-manager)'
   dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
   dnf install -y gh
+}
+
+# Prefixes the old version of the given function with "_" to make it callable
+# from the overwriting function.
+function prefix_function() {
+  local function_name=$1
+  eval "_`declare -f $function_name`"
 }
