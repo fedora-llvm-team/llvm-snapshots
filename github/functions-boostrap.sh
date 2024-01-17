9211f87372da547f168606ba21d837d45b77af09
Konrad Kleine <kkleine@redhat.com>
2024-01-17 18:07:55 +0100

    Build and check the bootstrap branch nightly on fedora-39-x86_64

---
+++ github/functions-boostrap.sh
@@ -0,0 +1,13 @@
+# This file contains overwrite for the functions in functions.sh
+set +x
+
+# Prints the chroots we care about.
+function get_chroots() {
+  echo "fedora-39-x86_64"
+}
+
+# Prints the packages we care about
+# overwrite
+function get_packages() {
+  echo "llvm"
+}
