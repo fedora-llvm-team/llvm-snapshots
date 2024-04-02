5ad9ac1ee03c6603e61785e9280997a45b336087
Konrad Kleine <kkleine@redhat.com>
2024-04-02 16:33:12 +0200

    Readjust the length to which texts' are shortened
    This fixes the broken test CI.

--- snapshot_manager/snapshot_manager/util.py
+++ snapshot_manager/snapshot_manager/util.py
@@ -147,7 +147,7 @@ def gunzip(f: tuple[str, pathlib.Path]) -> pathlib.Path:
     return pathlib.Path(str(f))
 
 
-def shorten_text(text: str, max_length: int = 1500) -> str:
+def shorten_text(text: str, max_length: int = 3000) -> str:
     """Truncates the given text to at most max_length.
 
     If we don't shorten log snippets, the github comment body will
