cdcda96172d8040d9cbc8cae8e6968ca70d982e9
Konrad Kleine <kkleine@redhat.com>
2024-06-26 16:55:16 +0200

    Remove not needed bits of old upstream-snapshot since big-merge is the way to go for now

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -56,12 +56,12 @@ function get_active_build_ids(){
 
 # Prints the chroots we care about.
 function get_chroots() {
-  copr list-chroots | grep -P '^fedora-(rawhide|[0-9]+)' | sort | tr '\n' ' '
+  copr list-chroots | grep -P '^(fedora-(rawhide|[0-9]+)|rhel-9-)' | sort |  tr '\n' ' '
 }
 
 # Prints the packages we care about
 function get_packages() {
-  echo "python-lit llvm clang lld compiler-rt libomp"
+  echo "llvm"
 }
 
 # Returns false if a package needs special handling on certain architectures
