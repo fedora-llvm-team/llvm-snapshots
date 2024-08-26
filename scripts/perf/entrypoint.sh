c71bf57417b1b68284879b72bc5d46b65a082d70
Konrad Kleine <kkleine@redhat.com>
2024-08-26 17:19:02 +0000

    compare the system llvm against "big-merge" and "pgo"

---
+++ scripts/perf/entrypoint.sh
@@ -0,0 +1,118 @@
+#!/usr/bin/bash
+
+set -x
+
+# Source the python environment with required packages
+source ~/mysandbox/bin/activate
+
+function configure_build_run {
+    # Configure the test suite
+    cmake \
+        -DCMAKE_GENERATOR=Ninja \
+        -DCMAKE_C_COMPILER=/usr/bin/clang \
+        -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
+        -C~/test-suite/cmake/caches/O3.cmake \
+        ~/test-suite
+
+    # Build the test-suite
+    ninja -j30
+
+    # Run the tests with lit:
+    lit -j1 -v -o results.json . || true
+}
+
+# Query version information for given day
+yyyymmdd=20240825
+git_rev=$(curl -sL https://github.com/fedora-llvm-team/llvm-snapshots/releases/download/snapshot-version-sync/llvm-git-revision-${yyyymmdd}.txt)
+git_rev_short="${git_rev:0:14}"
+llvm_release=$(curl -sL https://github.com/fedora-llvm-team/llvm-snapshots/releases/download/snapshot-version-sync/llvm-release-${yyyymmdd}.txt)
+rpm_suffix="${llvm_release}~pre${yyyymmdd}.g${git_rev_short}"
+
+echo "git_rev=$git_rev"
+echo "git_rev_short=$git_rev_short"
+echo "llvm_release=$llvm_release"
+echo "rpm_suffix=$rpm_suffix"
+
+######################################################################################
+# PGO
+######################################################################################
+
+# Install and enable the repository that provides the PGO LLVM Toolchain
+# See https://llvm.org/docs/HowToBuildWithPGO.html#building-clang-with-pgo
+dnf copr enable -y @fedora-llvm-team/llvm-snapshots-pgo-${yyyymmdd}
+dnf install -y \
+    clang-${rpm_suffix} \
+    clang-${rpm_suffix} \
+    clang-libs-${rpm_suffix} \
+    clang-resource-filesystem-${rpm_suffix} \
+    llvm-${rpm_suffix} \
+    llvm-libs-${rpm_suffix}
+
+mkdir -pv ~/pgo
+cd ~/pgo
+
+configure_build_run
+# Remove packages from that PGO repo and the repo itself
+dnf -y repository-packages copr:copr.fedorainfracloud.org:group_fedora-llvm-team:llvm-snapshots-pgo-${yyyymmdd} remove
+dnf -y copr remove @fedora-llvm-team/llvm-snapshots-pgo-${yyyymmdd}
+
+######################################################################################
+# big-merge
+######################################################################################
+
+# Install and enable the repository that provides the big-merge LLVM Toolchain
+dnf copr enable -y @fedora-llvm-team/llvm-snapshots-big-merge-${yyyymmdd}
+dnf install -y \
+    clang-${rpm_suffix} \
+    clang-${rpm_suffix} \
+    clang-libs-${rpm_suffix} \
+    clang-resource-filesystem-${rpm_suffix} \
+    llvm-${rpm_suffix} \
+    llvm-libs-${rpm_suffix}
+
+mkdir -pv ~/big-merge
+cd ~/big-merge
+
+configure_build_run
+# Remove packages from that big-merge repo and the repo itself
+dnf -y repository-packages copr:copr.fedorainfracloud.org:group_fedora-llvm-team:llvm-snapshots-big-merge-${yyyymmdd} remove
+dnf -y copr remove @fedora-llvm-team/llvm-snapshots-big-merge-${yyyymmdd}
+
+######################################################################################
+# system llvm
+######################################################################################
+
+# Build with regular clang
+dnf install -y clang clang-libs clang-resource-filesystem llvm llvm-libs
+mkdir -pv ~/system
+cd ~/system
+
+configure_build_run
+
+system_llvm_release=$(clang --version | grep -Po '[0-9]+\.[0-9]+\.[0-9]' | head -n1)
+
+/root/test-suite/utils/compare.py \
+    --metric exec_time \
+    --metric compile_time \
+    --metric link_time \
+    --lhs-name ${system_llvm_release} \
+    --rhs-name pgo-${rpm_suffix} \
+    ~/system/results.json vs ~/pgo/results.json > ~/results-system-vs-pgo.txt || true
+
+/root/test-suite/utils/compare.py \
+    --metric exec_time \
+    --metric compile_time \
+    --metric link_time \
+    --lhs-name ${system_llvm_release} \
+    --rhs-name big-merge-${rpm_suffix} \
+    ~/system/results.json vs ~/big-merge/results.json > ~/results-system-vs-big-merge.txt || true
+
+/root/test-suite/utils/compare.py \
+    --metric exec_time \
+    --metric compile_time \
+    --metric link_time \
+    --lhs-name big-merge-${rpm_suffix} \
+    --rhs-name pgo-${rpm_suffix} \
+    ~/big-merge/results.json vs ~/pgo/results.json > ~/results-big-merge-vs-pgo.txt || true
+
+bash
