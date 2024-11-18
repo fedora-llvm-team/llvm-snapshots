6c29f1daa065fcec681d98e5b8ac209ef6b4302e
Tom Stellard <tstellar@redhat.com>
2024-11-18 13:31:40 -0800

    Address review comments

--- .github/workflows/rebuilder.py
+++ .github/workflows/rebuilder.py
@@ -1,12 +1,25 @@
 import argparse
 import datetime
 import json
+import logging
 import re
 from typing import Set
 
 import copr.v3
 import dnf
 import hawkey
+
+
+def load_tests(loader, tests, ignore):
+    """We want unittest to pick up all of our doctests
+
+    See https://docs.python.org/3/library/unittest.html#load-tests-protocol
+    See https://stackoverflow.com/a/27171468
+    """
+    import doctest
+
+    tests.addTests(doctest.DocTestSuite())
+    return tests
 
 
 def filter_llvm_pkgs(pkgs: set[str]) -> set[str]:
@@ -41,7 +54,7 @@ def filter_llvm_pkgs(pkgs: set[str]) -> set[str]:
         "flang",
         "mlir",
     }    
-    llvm_pkg_pattern=rf"({"|".join(llvm_pkgs)})[0-9]*$"
+    llvm_pkg_pattern=rf"({'|'.join(llvm_pkgs)})[0-9]*$"
     return {pkg for pkg in pkgs if not re.match(llvm_pkg_pattern, pkg)}
 
 
@@ -72,19 +85,23 @@ def get_pkgs(exclusions: set[str]) -> set[set]:
     q = base.sack.query(flags=hawkey.IGNORE_MODULAR_EXCLUDES)
     q = q.available()
     q = q.filter(requires=["clang", "gcc", "gcc-c++"])
-    pkgs = {[p.name for p in list(q)]}
-    return filter_llvm_pkgs(pkgs) - exclusions
+    pkgs = [p.name for p in list(q)]
+    return filter_llvm_pkgs(set(pkgs)) - exclusions
 
 
-def get_monthly_rebuild_packages(
-    project_owner: str, project_name: str, copr_client: copr.v3.Client, pkgs: set[str]
-) -> set[str]:
-    for p in copr_client.package_proxy.get_list(
+def get_builds_from_copr(
+    project_owner: str, project_name: str, copr_client: copr.v3.Client
+) -> list[dict]:
+    return copr_client.package_proxy.get_list(
         project_owner,
         project_name,
         with_latest_succeeded_build=True,
         with_latest_build=True,
-    ):
+    )
+
+def get_monthly_rebuild_packages(pkgs: set[str], copr_pkgs: list[dict]
+) -> set[str]:
+    for p in copr_pkgs:
         latest_succeeded = p["builds"]["latest_succeeded"]
         latest = p["builds"]["latest"]
         if p["name"] not in pkgs:
@@ -100,16 +117,38 @@ def get_monthly_rebuild_packages(
 def get_monthly_rebuild_regressions(
     project_owner: str,
     project_name: str,
-    copr_client: copr.v3.Client,
     start_time: datetime.datetime,
+    copr_pkgs: list[dict]
 ) -> set[str]:
+    """Returns the list of packages that failed to build in the most recent
+       rebuild, but built successfully in the previous rebuild.
+    
+    Args:
+        start_time (datetime.datetime): The start time of the most recent mass
+                                        rebuild.  This needs to be a time
+                                        before the most recent mass rebuild
+                                        and after the previous one.
+        copr_pkgs (list[dict]): List of built packages for the COPR project.
+
+    Returns:
+        set[str]: List of packages that regressed in the most recent rebuilt.
+
+    Example:
+   
+    >>> a = {"name" : "a", "latest" : { "id" : 1, "state" : "running", "submitted_on" : 1731457321 } , "latest_succeeded" : None }
+    >>> b = {"name" : "b", "latest" : { "id" : 1, "state" : "succeeded", "submitted_on" : 1731457321 } , "latest_succeeded" : None }
+    >>> c = {"name" : "c", "latest" : { "id" : 1, "state" : "succeeded", "submitted_on" : 1731457321 } , "latest_succeeded" : { "id" : 1 } }
+    >>> d = {"name" : "d", "latest" : { "id" : 2, "state" : "canceled", "submitted_on" : 1731457321 } , "latest_succeeded" : { "id" : 1 } }
+    >>> e = {"name" : "e", "latest" : { "id" : 2, "state" : "failed", "submitted_on" : 1 } , "latest_succeeded" : { "id" : 1 } }
+    >>> f = {"name" : "f", "latest" : { "id" : 2, "state" : "failed", "submitted_on" : 1731457321 } , "latest_succeeded" : { "id" : 1 } }
+    >>> copr_pkgs=[ a, b, c, d, e, f ]
+    >>> regressions = get_monthly_rebuild_regressions(datetime.datetime.fromisoformat("2024-11-11", copr_pkgs)
+    >>> print(regressions)
+    { 'f' }
+    
+    """
     pkgs = []
-    for p in copr_client.package_proxy.get_list(
-        project_owner,
-        project_name,
-        with_latest_succeeded_build=True,
-        with_latest_build=True,
-    ):
+    for p in copr_pkgs:
         latest_succeeded = p["builds"]["latest_succeeded"]
         latest = p["builds"]["latest"]
 
@@ -127,7 +166,7 @@ def get_monthly_rebuild_regressions(
             continue
         if latest["id"] == latest_succeeded["id"]:
             continue
-        # latest is a bit a successful build, but this doesn't mean it failed.
+        # latest is a successful build, but this doesn't mean it failed.
         # It could be in progress.
         if latest["state"] != "failed":
             continue
@@ -164,9 +203,9 @@ def start_rebuild(
     buildopts = {
         "background": True,
     }
-    print("Rebuilding", len(pkgs), "packages")
+    logging.info("Rebuilding", len(pkgs), "packages")
     for p in pkgs:
-        print("Rebuild", p)
+        logging.info("Rebuild", p)
         copr_client.build_proxy.create_from_distgit(
             project_owner, project_name, p, "f41", buildopts=buildopts
         )
@@ -182,7 +221,7 @@ def select_snapshot_project(
         chroots = set()
         day = datetime.date.today() - datetime.timedelta(days=i)
         project_name = day.strftime("llvm-snapshots-big-merge-%Y%m%d")
-        print("Trying:", project_name)
+        logging.info("Trying:", project_name)
         try:
             p = copr_client.project_proxy.get(project_owner, project_name)
             if not p:
@@ -193,13 +232,13 @@ def select_snapshot_project(
             for pkg in pkgs:
                 chroots.update(pkg["chroots"])
 
-            print(project_name, chroots)
+            logging.info(project_name, chroots)
             if all(t in chroots for t in target_chroots):
-                print("PASS", project_name)
+                logging.info("PASS", project_name)
                 return project_name
         except:
             continue
-    print("FAIL")
+    logging.warn("FAIL")
     return None
 
 
@@ -243,18 +282,16 @@ def main():
         pkgs = get_pkgs(exclusions)
         try:
             copr_client.project_proxy.get(project_owner, project_name)
-            pkgs = get_monthly_rebuild_packages(
-                project_owner, project_name, copr_client, pkgs
-            )
+            copr_pkgs = get_builds_from_copr(project_owner, project_name, copr_client)
+            pkgs = get_monthly_rebuild_packages(pkgs, copr_pkgs)
         except:
             create_new_project(project_owner, project_name, copr_client, target_chroots)
         snapshot_project = select_snapshot_project(copr_client, target_chroots)
         start_rebuild(project_owner, project_name, copr_client, pkgs, snapshot_project)
     elif args.command == "get-regressions":
         start_time = datetime.datetime.fromisoformat(args.start_date)
-        pkg_failures = get_monthly_rebuild_regressions(
-            project_owner, project_name, copr_client, start_time
-        )
+        copr_pkgs = get_builds_from_copr(project_owner, project_name, copr_client)
+        pkg_failures = get_monthly_rebuild_regressions(project_owner, project_name, start_time, copr_pkgs)
         print(json.dumps(pkg_failures))
 
 
