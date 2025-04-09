d8c8f25982f154ade067214bd18d127cc2a1e662
Tom Stellard <tstellar@redhat.com>
2025-04-08 22:51:10 -0700

    git-bisect-script.sh: Drop fedora-clang-default-cc dependency
    We can select the clang toolchain on the rpmbuild command line so
    we don't need this package.

--- scripts/git-bisect-script.sh
+++ scripts/git-bisect-script.sh
@@ -4,4 +4,4 @@ srpm_name=$1
 
 ninja -C build install-clang install-clang-resource-headers install-LLVMgold install-llvm-ar install-llvm-ranlib
 
-rpmbuild -rb $srpm_name
+rpmbuild -D '%toolchain clang' -rb $srpm_name
