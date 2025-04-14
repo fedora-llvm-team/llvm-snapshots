94d042ad99a8858d60ab4021dd591c6a3b699a51
Konrad Kleine <kkleine@redhat.com>
2025-04-14 22:11:45 +0200

    Mixed cleanups
    Some of the refactorings done in previous PRs required changes that were
    not directly tied to functionality but refactoring. To close out this
    series of PRs, this does fixes in places that I wanted to exclude for
    readability reasons in previous PRs.
    
    ghstack-source-id: e0345861579343415e824f7bd65a83d7d7561d89
    Pull Request resolved: https://github.com/fedora-llvm-team/llvm-snapshots/pull/1282

---
+++ snapshot_manager/snapshot_manager/__init__.py
@@ -0,0 +1,16 @@
+"""
+isort:skip_file
+"""
+
+__all__ = [
+    "build_status",
+    "config",
+    "copr_util",
+    "file_access",
+    "github_graphql",
+    "github_util",
+    "performance_diagrams",
+    "snapshot_manager",
+    "util",
+]
+from snapshot_manager.snapshot_manager import SnapshotManager
