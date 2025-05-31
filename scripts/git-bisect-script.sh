ebea192295f2f28a6900a04e60bd367a6c9588c3
Tom Stellard <tstellar@redhat.com>
2025-05-30 22:08:47 -0700

    git-bisect-script.sh: Exit with status 125 when build fails (#1414)
    This tells git bisect that the commit is not buildable.

--- scripts/git-bisect-script.sh
+++ scripts/git-bisect-script.sh
@@ -2,6 +2,8 @@
 
 srpm_name=$1
 
-ninja -C build install-clang install-clang-resource-headers install-LLVMgold install-llvm-ar install-llvm-ranlib
+if ! ninja -C build install-clang install-clang-resource-headers install-LLVMgold install-llvm-ar install-llvm-ranlib; then
+  exit 125
+fi
 
 rpmbuild -D '%toolchain clang' -rb $srpm_name
