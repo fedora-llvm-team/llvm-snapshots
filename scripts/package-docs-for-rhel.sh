09492052a79a9e64bde07db53e514a88af37082f
Konrad Kleine <kkleine@redhat.com>
2026-05-29 10:58:18 +0200

    Extract SRPM to <base_dir>/srpm and parse spec file for doc files
    Fixes #2092 - Extract the SRPM (NOT only the llvm.spec) to some directory.
    Fixes #2093 - Parse the spec file for /usr/share/man files

--- scripts/package-docs-for-rhel.sh
+++ scripts/package-docs-for-rhel.sh
@@ -29,7 +29,7 @@ function download_rpms {
     local arch=${2:-x86_64}
     local target_dir=${3:-./$PWD/${arch}}
 
-    echo "INFO: Downloading RPMS for ${nvr} and arch ${arch} to ${target_dir}"
+    echo "INFO: Downloading RPMS for ${nvr} and arch ${arch} to ${target_dir}" 1>&2
     mkdir -p "${target_dir}"
     pushd "${target_dir}"
     koji download-build --arch="${arch}" "${nvr}"
@@ -43,10 +43,40 @@ function download_srpm {
     local target_dir=${2:-$PWD}
     local srpm="${nvr}.src.rpm"
 
-    echo "INFO: Downloading ${srpm} to ${target_dir}"
+    echo "INFO: Downloading ${srpm} to ${target_dir}" 1>&2
     mkdir -p "${target_dir}"
     pushd "$target_dir"
     koji download-build --rpm "$srpm"
+    popd
+}
+
+# Extracts a given SRPM file to a given target directory.
+function extract_srpm {
+    local srpm=${1:-$(get_nvr).src.rpm}
+    local target_dir=${2:-${PWD}/srpm}
+
+    echo "INFO: Extracting SRPM ${srpm} to ${target_dir}" 1>&2
+    mkdir -p "${target_dir}"
+    pushd "${target_dir}"
+    bsdtar xf "${srpm}"
+    popd
+}
+
+# Prints all files belonging to man page documentation from the fiven spec file
+# for a given RHEL version.
+function parse_spec_file_for_doc_files {
+    local spec_file=${1:-${PWD}/srpm/llvm.spec}
+    local rhel_version=${2:-UNDEFINED_RHEL_VERSION}
+    local spec_dir
+
+    echo "INFO: Parsing spec file ${spec_file} for doc files" 1>&2
+    spec_dir=$(dirname "${spec_file}")
+    pushd "${spec_dir}"
+    rpmspec \
+        --undefine=fedora \
+        --define="rhel ${rhel_version}" \
+        -P llvm.spec 2>/dev/null \
+    | grep -P '^/usr/share/man'
     popd
 }
 
@@ -57,18 +87,21 @@ function main {
     local base_dir="${PWD}/package-rhel-docs"
     local final_dir="${base_dir}/final"
     local rhel_version=${1:-9}
+    local doc_files=${base_dir}/doc_files.txt
 
     rawhide_tag=$(get_rawhide_tag)
     nvr=$(get_nvr "${rawhide_tag}")
     mkdir -p "${base_dir}"
     mkdir -p "${final_dir}"
 
-    echo "INFO: Architecture: ${arch}"
-    echo "INFO: Artifacts: ${final_dir}"
-    echo "INFO: Target RHEL version: ${rhel_version}"
+    echo "INFO: Architecture: ${arch}" 1>&2
+    echo "INFO: Artifacts: ${final_dir}" 1>&2
+    echo "INFO: Target RHEL version: ${rhel_version}" 1>&2
 
     download_rpms "${nvr}" "${arch}" "${base_dir}"
     download_srpm "${nvr}" "${base_dir}"
+    extract_srpm "${base_dir}/${nvr}.src.rpm" "${base_dir}/srpm"
+    parse_spec_file_for_doc_files "${base_dir}/srpm/llvm.spec" "${rhel_version}" > "${doc_files}"
 }
 
 main
