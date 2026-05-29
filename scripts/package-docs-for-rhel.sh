c66c5044cf999b776bd0a1cc0aa97885266a21cf
Konrad Kleine <kkleine@redhat.com>
2026-05-29 23:23:13 +0200

    Enquote parameters to avoid unwanted expansion or splitting

--- scripts/package-docs-for-rhel.sh
+++ scripts/package-docs-for-rhel.sh
@@ -22,7 +22,7 @@ function get_rawhide_tag {
 # given tag (e.g. f45). If no tag is provided, it will be determined
 # automatically from whatever tag is assigned to rawhide atm.
 function get_nvr {
-    local tag=${1:-$(get_rawhide_tag)}
+    local tag="${1:-$(get_rawhide_tag)}"
     local nvr
 
     nvr=$(koji latest-build --quiet "${tag}" llvm | cut -d ' ' -f1)
@@ -32,9 +32,9 @@ function get_nvr {
 
 # Downloads the RPMs for a given NVR and architecture into a target directory.
 function download_rpms {
-    local nvr=${1:-$(get_nvr)}
-    local arch=${2:-x86_64}
-    local target_dir=${3:-./$PWD/${arch}}
+    local nvr="${1:-$(get_nvr)}"
+    local arch="${2:-x86_64}"
+    local target_dir="${3:-./$PWD/${arch}}"
 
     echo "INFO: Downloading RPMS for ${nvr} and arch ${arch} to ${target_dir}" 1>&2
     mkdir -p "${target_dir}"
@@ -46,8 +46,8 @@ function download_rpms {
 # Downloads the SRPM for the given NVR so we can extract the llvm.spec file
 # later.
 function download_srpm {
-    local nvr=${1:-$(get_nvr)}
-    local target_dir=${2:-$PWD}
+    local nvr="${1:-$(get_nvr)}"
+    local target_dir="${2:-$PWD}"
     local srpm="${nvr}.src.rpm"
 
     echo "INFO: Downloading ${srpm} to ${target_dir}" 1>&2
@@ -59,8 +59,8 @@ function download_srpm {
 
 # Extracts a given SRPM file to a given target directory.
 function extract_srpm {
-    local srpm=${1:-$(get_nvr).src.rpm}
-    local target_dir=${2:-${PWD}/srpm}
+    local srpm="${1:-$(get_nvr).src.rpm}"
+    local target_dir="${2:-${PWD}/srpm}"
 
     echo "INFO: Extracting SRPM ${srpm} to ${target_dir}" 1>&2
     mkdir -p "${target_dir}"
@@ -72,10 +72,10 @@ function extract_srpm {
 # Writes all files belonging to man page documentation from the given spec file
 # for a given RHEL version to the given destination file list.
 function parse_spec_file_for_doc_files {
-    local spec_file=${1:-${PWD}/srpm/llvm.spec}
-    local rhel_version=${2:-UNDEFINED_RHEL_VERSION}
+    local spec_file="${1:-${PWD}/srpm/llvm.spec}"
+    local rhel_version="${2:-UNDEFINED_RHEL_VERSION}"
     local spec_dir
-    local dest_file_list=${3}
+    local dest_file_list="${3}"
 
     echo "INFO: Parsing spec file ${spec_file} for doc files" 1>&2
     spec_dir=$(dirname "${spec_file}")
@@ -91,9 +91,9 @@ function parse_spec_file_for_doc_files {
 # Extracts all files from the given rpm that are in the files list to the given
 # target directory.
 function extract_files_from_rpm {
-    local rpm=${1}
-    local target_dir=${2}
-    local files_list=${3:-doc-files.txt}
+    local rpm="${1}"
+    local target_dir="${2}"
+    local files_list="${3:-doc-files.txt}"
 
     echo "INFO: Extracting files from ${rpm} in ${target_dir}" 1>&2
     mkdir -p "${target_dir}"
@@ -109,7 +109,7 @@ function main {
     local nvr
     local arch=x86_64
     local base_dir="${PWD}/package-rhel-docs"
-    local rhel_version=${1:-9}
+    local rhel_version="${1:-9}"
     local doc_files="${base_dir}/doc_files.txt"
     local doc_files_cpio="${base_dir}/doc_files_cpio.txt"
 
